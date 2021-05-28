8c4ff7a3b868af0dd5e6043fac8998fa67762782
sbarcenas <sbarcenas@sophilabs.com>
2021-05-28 18:04:39 -0500

    First commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ArticlesModule } from './articles/articles.module';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ArticlesModule, DatabaseModule],
+  imports: [UsersModule, ArticlesModule, DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
